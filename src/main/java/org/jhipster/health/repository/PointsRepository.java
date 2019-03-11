81b705f70bf2a446b1c243a09e84f206c8d3c94e
Jiweon Park <jiweon.park@gracehanin.org>
2019-03-10 18:53:27 -0700

    first commit

---
+++ src/main/java/org/jhipster/health/repository/PointsRepository.java
@@ -0,0 +1,24 @@
+package org.jhipster.health.repository;
+
+import org.jhipster.health.domain.Points;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.*;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/**
+ * Spring Data  repository for the Points entity.
+ */
+@SuppressWarnings("unused")
+@Repository
+public interface PointsRepository extends JpaRepository<Points, Long> {
+
+    @Query("select points from Points points where points.user.login = ?#{principal.username}")
+    Page<Points> findByUserIsCurrentUser(Pageable pageable);
+
+    @Query("select points from Points points where points.user.login = ?#{principal.username} order by points.date desc")
+    Page<Points> findAllByOrderByDateDesc(Pageable pageable);
+
+}
